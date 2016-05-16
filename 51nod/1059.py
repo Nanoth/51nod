9e1fc78234a819b18ac37b6f94ccdbdda0824531
Nanoth <444051232@qq.com>
2016-05-16 16:15:48 +0800

    Create 1059.py

---
+++ 51nod/1059.py
@@ -0,0 +1,17 @@
+
+
+def solve(l,r):
+    if l==r:    return l
+    m=(l+r)//2
+    return solve(l,m)*solve(m+1,r)
+
+def print_line(x):
+    x=str(x)
+    for i in range(0,len(x),1000):
+        j=i+1000
+        if j>len(x): j=len(x)
+        print x[i:j]
+
+if __name__ == '__main__':
+    a=int(input())
+    print_line(solve(1,a))
